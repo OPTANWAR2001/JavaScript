d89643a86e6c29efc47566002d88df78ed4069ff
OPTANWAR2001 <optanwar10@gmail.com>
2024-03-10 01:29:37 +0530

    Add js

--- js/array/MAx_value_arr.js
+++ js/array/MAx_value_arr.js
@@ -1,1 +1,27 @@
-console.log("HElo ");
+let arr = [11, 2, 15, 3, 1, 6, 3, 9];
+
+//! Find Max using for loop
+let maxNum = 0;
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  if (maxNum < arr[i]) {
+    maxNum = arr[i];
+  }
+}
+
+console.log("maxNum", maxNum);
+
+//! Using Sort Method
+
+let newArr = arr.sort((a, b) => a - b);
+console.log("newArr", newArr[arr.length - 1]);
+
+//Decending
+let newArrs = arr.sort((a, b) => b - a);
+console.log("newArrsss", newArrs[0]);
+
+// ! using Math.max()
+
+let maxNumber = Math.max(...arr);
+
+console.log("maxNumber", maxNumber);
---
+++ js/string/ReverseStringWithoutANyMethos.js
@@ -0,0 +1,8 @@
+let str = "Hello World";
+let revStr = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  revStr += str[i];
+}
+
+console.log("revStr", revStr);
