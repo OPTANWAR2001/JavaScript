da5cf6ed4d2c752eb0ba4f2c9d37bbd14c6afbf0
OPTANWAR2001 <optanwar10@gmail.com>
2024-03-16 04:26:34 +0530

    remove image

--- js/string/ReverseStringWithoutANyMethos.js
+++ js/string/ReverseStringWithoutANyMethos.js
@@ -6,3 +6,15 @@ for (let i = str.length - 1; i >= 0; i--) {
 }
 
 console.log("revStr", revStr);
+
+function sum() {
+  return function (a) {
+    return function (b) {
+      return function (c) {
+        console.log("sum is", a + b + c);
+      };
+    };
+  };
+}
+
+sum()(2)(3)(4);
