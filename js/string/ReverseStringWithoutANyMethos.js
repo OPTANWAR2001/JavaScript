7018c6fa520efd3531a31b0ee4c86470bf74afa7
OPTANWAR2001 <optanwar10@gmail.com>
2024-03-17 07:28:28 +0530

    add new questions

--- js/string/ReverseStringWithoutANyMethos.js
+++ js/string/ReverseStringWithoutANyMethos.js
@@ -1,19 +1,1 @@
-let str = "Hello JavaScript";
-let revStr = "";
 
-for (let i = str.length - 1; i >= 0; i--) {
-  revStr += str[i];
-}
-
-console.log("revStr", revStr);
-
-function add() {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        console.log("sum is", a + b + c);
-      };
-    };
-  };
-}
-add()(2)(3)(5);
